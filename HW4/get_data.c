48c97f4a8814fa266d42cc9a2b8df8ed628ee5d5
Jose Jimenez <jomjimenezme@unal.edu.co>
2020-12-03 09:08:35 +0100

    some indentation made in HW4 excercises

--- HW4/get_data.c
+++ HW4/get_data.c
@@ -5,25 +5,25 @@
 
 void Get_data( double* a_ptr, double* b_ptr, int* n_ptr, int my_rank, int p){
 
-	int source =0;
-	int dest;
-	int tag;
+  int source =0;
+  int dest;
+  int tag;
 
-	MPI_Status status;
+  MPI_Status status;
 
-	if(my_rank ==0){
+  if(my_rank ==0){
 
-    	FILE *input;
-        input = fopen("input.d", "r");
-        fscanf(input, "%lf", a_ptr);
-        fscanf(input, "%lf", b_ptr);	
-        fscanf(input, "%d", n_ptr);	
-        fclose(input);
+    FILE *input;
+    input = fopen("input.d", "r");
+    fscanf(input, "%lf", a_ptr);
+    fscanf(input, "%lf", b_ptr);	
+    fscanf(input, "%d", n_ptr);	
+    fclose(input);
 
-	}
+  }
 
-	MPI_Bcast(a_ptr, 1,MPI_DOUBLE, 0	, MPI_COMM_WORLD);
-	MPI_Bcast(b_ptr, 1, MPI_DOUBLE, 0	, MPI_COMM_WORLD);
-	MPI_Bcast(n_ptr, 1, MPI_INT, 0	, MPI_COMM_WORLD);
+  MPI_Bcast(a_ptr, 1,MPI_DOUBLE, 0	, MPI_COMM_WORLD);
+  MPI_Bcast(b_ptr, 1, MPI_DOUBLE, 0	, MPI_COMM_WORLD);
+  MPI_Bcast(n_ptr, 1, MPI_INT, 0	, MPI_COMM_WORLD);
 }
 
