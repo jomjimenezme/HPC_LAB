88b826ad6c10d19b511456d7c508cccdb4f4274b
m1941820 <m1941820@frontend.stromboli.physik.uni-wuppertal.de>
2020-12-06 15:05:02 +0100

    HW5: We are able to distribute a matrix of any size for any number of processors

--- HW5/ex2.c
+++ HW5/ex2.c
@@ -13,20 +13,28 @@ int main(int argc, char** argv){
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
   Read_size(&m, &n, my_rank, p);
-   local_m=m/p;
+ //printf("hhhhhhhhhhhhhhhhheyyy! m is %d  ",m); 
+  local_m=m/p;
+  if(m%p!=0){
+    if(my_rank<m%p){
+	local_m = local_m +1;
+    }
+  }
   local_n=n;
   
-  local_x= malloc( local_n *sizeof( double ) );
-  Read_vector(local_x, local_n, my_rank, p);
+
+  //printf("Rank %d, m_local=%d\n", my_rank, local_m);
+  local_x= malloc( local_m *sizeof( double ) );
+  //Read_vector(local_x, local_m, 1 , my_rank, p);
   
+  //printf("Hello World, I am proces %d out of %d processes  %lf\n", my_rank, p,local_x[0]); 
   local_A= malloc( local_m*local_n *sizeof( double ) );
-  Read_matrix(local_A, local_m, local_n, my_rank, p);
+  Read_matrix(local_A, m, n, my_rank, p);
   
-
-  printf("%d %lf %lf \n", my_rank, local_A[0], local_A[7]);
-
+if(my_rank==0){
+  printf("%d %lf %lf \n", my_rank, local_A[0], local_A[15]);
+}
  // printf("Hello World, I am proces %d out of %d processes     %d  %d\n", my_rank, p,m, n); 
-  // printf("Hello World, I am proces %d out of %d processes     %lf  %lf\n", my_rank, p,local_x[0], local_x[1]); 
 
   
   free(local_x);
--- HW5/get_data.c
+++ HW5/get_data.c
@@ -14,10 +14,97 @@ void Read_size(int* m_ptr, int* n_ptr, int my_rank, int p){
   }
   MPI_Bcast(m_ptr,1, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Bcast(n_ptr,1, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void Read_vector(double local_A[], int m, int n, int my_rank, int p){
+
+  int i,j;
+  int local_m, local_n=n;
+  int dest=0;
+  MPI_Status status;  
+  local_m=m/p;
+      if(m%p!=0){
+	if(dest<m%p){
+	  local_m = local_m+1;
+	}
+      }
+
+  if(my_rank==0){  
+    FILE *matrix;
+    matrix =fopen("vector.d","r");
+    while(dest<p){
+    local_m=m/p;
+      if(m%p!=0){
+	if(dest<m%p){
+	  local_m = local_m+1;
+	}
+      }
+      for(i=0; i<local_m; i++){ 
+        for(j=0; j<local_n; j++){
+          fscanf(matrix, "%lf", &local_A[i*local_m+j]);
+        }
+          MPI_Send(local_A,local_m*local_n, MPI_DOUBLE, dest, dest, MPI_COMM_WORLD);
+	  dest++; 
+      }
+    }
+    fclose(matrix);
+  }  
+
+    MPI_Recv(local_A, local_m*local_n, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD, &status);
+
 
 }
 
 
+void Read_matrix(double local_A[], int m, int n, int my_rank, int p){
+ int i,j;
+  int local_m, local_n=n;
+  int dest=0;
+  MPI_Status status;
+    
+  if(my_rank==0){  
+    FILE *matrix;
+    matrix =fopen("matrix.d","r");
+    while(dest<p){
+    local_m=m/p;
+      if(m%p!=0){
+	if(dest<m%p){
+	  local_m = local_m+1;
+	}
+      }
+      for(i=0; i<local_m; i++){ 
+        for(j=0; j<local_n; j++){
+          fscanf(matrix, "%lf", &local_A[i*local_n+j]);
+        }
+		//printf("%lf, %lf, %lf, %lf \n", local_A[0], local_A[1], local_A[2], local_A[3]);
+
+      }
+
+          MPI_Send(local_A,local_m*local_n, MPI_DOUBLE, dest, dest, MPI_COMM_WORLD);
+
+	  dest++; 
+//printf("Heeey rank=%d  m=%d  localm=%d \n", my_rank,m,local_m);
+    }
+    fclose(matrix);
+  }
+ local_m=m/p;
+      if(m%p!=0){
+	if(my_rank<m%p){
+	  local_m = local_m+1;
+	}
+      }
+ 
+//printf("Heeey rank=%d  m=%d  localm=%d \n", my_rank,m,local_m);
+    MPI_Recv(local_A, local_m*local_n, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD, &status);
+
+
+ }
+
+
+
+
+
+/*
 void Read_vector(double local_x[],int local_n, int my_rank, int p){
   int i;
   int n=local_n*p;
@@ -36,39 +123,4 @@ void Read_vector(double local_x[],int local_n, int my_rank, int p){
   MPI_Scatter(temp, local_n, MPI_DOUBLE, local_x, local_n, MPI_DOUBLE,0, MPI_COMM_WORLD);
 
   free(temp);
-}
-
-void Read_matrix(double local_A[], int local_m, int local_n, int my_rank, int p){
-
-  int i,j;
-  //int n=local_n*p;
-  //int m= local_m*p;
-  int dest=0;
-  //MPI_Request request, request2;
-  MPI_Status status;
-  //Dynamically allocation of vector
-  //temp = malloc( local_n*local_m * sizeof(double) ) ;
-
-  if(my_rank==0){  
-    FILE *matrix;
-    matrix =fopen("matrix.d","r");
-    while(dest<p){
-      for(i=0; i<local_m; i++){ 
-        for(j=0; j<local_n; j++){
-          fscanf(matrix, "%lf", &local_A[i+j]);
-        }
-        //if(local_m%j==0){
-		printf("%lf, %lf, %lf, %lf \n", local_A[0], local_A[1], local_A[2], local_A[3]);
-          MPI_Send(local_A,local_m*local_n, MPI_DOUBLE, dest, dest, MPI_COMM_WORLD);
-          //MPI_Isend(&local_A, local_m*local_n, MPI_DOUBLE, dest, dest, MPI_COMM_WORLD, &request); 
-	  dest++; 
-        //}
-      }
-        //MPI_Irecv(&local_A, local_m*local_n, MPI_DOUBLE, dest, dest, MPI_COMM_WORLD, &request2); 
-    }
-    fclose(matrix);
-  }  
-
-    MPI_Recv(local_A, local_m*local_n, MPI_DOUBLE, 0, my_rank, MPI_COMM_WORLD, &status);
-
-}
+}*/
