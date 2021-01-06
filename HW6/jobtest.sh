478582c30284e68cff4d5528a6edc1d1f45c0293
m1941820 <m1941820@frontend.stromboli.physik.uni-wuppertal.de>
2021-01-06 12:35:04 +0100

    HW6: Parallel multiplication debbuged, running for all test cases

--- HW6/jobtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --exclude=node05
-#SBATCH --exclusive
-#SBATCH --partition=NODE2008
-   mpirun  --mca btl_openib_allow_ib true ./a.out  >C.txt
